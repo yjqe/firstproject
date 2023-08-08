ea6e561ead818f81cebab541b6b585683e97ab3e
Yunjae Cho <yunjoew@gmail.com>
2023-08-08 15:11:34 +0900

    4일차 ~3.2.7 3.2.1 입력 폼 만들기 3.2.2 컨트롤러 만들기 3.2.3 폼 데이터 전송하기 - 뷰 파일 form 태그에 action, method 추가 3.2.4 폼 데이터 받기 - 컨트롤러에 @PostMapping 메서드 선언 3.2.5 DTO 만들기 - dto 패키지에 ArticleForm DTO 생성 3.2.6 폼 데이터를 DTO에 담기 - 컨트롤러 @PostMapping 메서드의 매개변수에 ArticleForm 객체 등록 3.2.7 입력 폼과 DTO 필드 연결하기 - 뷰 파일의 form 태그에 DTO 필드명과 동일한 이름을 name의 속성 값으로 등록

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -1,12 +1,20 @@
 package com.example.firstproject.controller;
 
+import com.example.firstproject.dto.ArticleForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ArticleController {
     @GetMapping("/articles/new")
-    public String newArtivleForm() {
+    public String newArticleForm() {
         return "articles/new";
+    }
+
+    @PostMapping("/articles/create")
+    public String createArticle(ArticleForm form) {  // 폼 데이터를 DTO로 받기
+        System.out.println(form.toString());  // DTO에 폼 데이터가 잘 담겼는지 확인
+        return "";
     }
 }
