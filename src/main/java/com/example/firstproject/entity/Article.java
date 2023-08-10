c824f87257646918117da4ce14cb5d1ee16d14a3
Yunjae Cho <yunjoew@gmail.com>
2023-08-10 20:52:33 +0900

    6일차 ~3장 3.4.1 H2 DB 접속하기 3.4.2 데이터 조회하기

--- src/main/java/com/example/firstproject/entity/Article.java
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -7,12 +7,13 @@ import jakarta.persistence.Id;
 
 @Entity
 public class Article {
-       // Article 생성자 추가
+    // Article 생성자 추가
     public Article(Long id, String title, String content) {
         this.id = id;
         this.title = title;
         this.content = content;
     }
+
     // toString 메서드 추가
     @Override
     public String toString() {
