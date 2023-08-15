41e44a5bc14334660bc77fbce537aab7ee2c7b0b
Yunjae Cho <yunjoew@gmail.com>
2023-08-15 16:33:01 +0900

    11일차 ~8장 게시글 삭제하기: DELETE, <셀프체크> 회원 가입 페이지의 수정, 삭제 기능 구현 8.2.5 최종 정리 <셀프체크> 회원 가입 페이지의 수정, 삭제 기능 구현하기

--- src/main/java/com/example/firstproject/controller/MemberController.java
+++ src/main/java/com/example/firstproject/controller/MemberController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -62,6 +63,49 @@ public class MemberController {
 
         // 3. 사용자에게 보여줄 뷰 페이지 설정하기
         return "members/index";
+    }
+
+    @GetMapping("/members/{id}/edit")
+    public String edit(@PathVariable Long id, Model model) {
+        // 수정할 데이터 가져오기
+        Member memberEntity = memberRepository.findById(id).orElse(null);
+        // 모델에 데이터 등록하기
+        model.addAttribute("member", memberEntity);
+        // 뷰 페이지 설정하기
+        return "members/edit";
+    }
+
+    @PostMapping("/members/update")
+    public String update(MemberForm form) { // 매개변수로 DTO 받아 오기
+        //수정 폼 DTO를 잘 받았는지 로그를 통해 확인
+        log.info(form.toString());
+        // 1. DTO를 엔티티로 변환하기
+        Member memberEntity = form.toEntity();
+        log.info(memberEntity.toString());
+        // 2. 엔티티를 DB에 저장하기
+        // 2-1. DB에서 기존 데이터 가져오기
+        Member target = memberRepository.findById(memberEntity.getId()).orElse(null);
+        // 2-2. 기존 데이터 값을 갱신하기
+        if (target != null) {
+            memberRepository.save(memberEntity);
+        }
+        // 3. 수정 결과 페이지로 리다이렉트하기
+        return "redirect:/members/" + memberEntity.getId();
+    }
+
+    @GetMapping("/members/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("삭제 요청이 들어왔습니다.");
+        // 1. 삭제할 대상 가져오기
+        Member target = memberRepository.findById(id).orElse(null);
+        log.info(target.toString());
+        // 2. 대상 엔티티 삭제하기
+        if (target != null) {
+            memberRepository.delete(target);
+            rttr.addFlashAttribute("msg", "삭제됐습니다.");
+        }
+        // 3. 결과 페이지로 리다이렉트하기
+        return "redirect:/members";
     }
 }
 
--- src/main/java/com/example/firstproject/dto/MemberForm.java
+++ src/main/java/com/example/firstproject/dto/MemberForm.java
@@ -7,11 +7,12 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class MemberForm {
+    private Long id;
     private String email;
     private String password;
 
     // 생성자(롬복)
     // toString 오버라이드(롬복)
 
-    public Member toEntity() { return new Member(null, email, password); }
+    public Member toEntity() { return new Member(id, email, password); }
 }
