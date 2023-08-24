9303276a55cc21c317a8a497f1e8eb62e2fadf04
Yunjae Cho <yunjoew@gmail.com>
2023-08-24 21:52:49 +0900

    19일차 ~15.3 댓글 조회하기 15.1 댓글 REST API의 개요 15.2 댓글 컨트롤러와 서비스 틀 만들기 15.3 댓글 조회하기 15.3.1 요청을 받아 응답할 컨트롤러 만들기 15.3.2 요청을 처리할 서비스 만들기 15.3.3 결과 확인하기

---
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -0,0 +1,37 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Comment;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        /*// 1. 댓글 조회
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+        // 2. 엔티티 -> DTO 변환
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        for (int i = 0; i < comments.size(); i++) {
+            Comment c = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(c);
+            dtos.add(dto);
+        }*/
+        // 3. 결과 반환
+        return commentRepository.findByArticleId(articleId)
+                .stream() // 댓글 엔티티 목록을 스트림으로 변환
+                .map(comment -> CommentDto.createCommentDto(comment)) // 엔티티를 DTO로 매핑
+                .collect(Collectors.toList()); // 스트림을 리스트로 변환
+    }
+}
